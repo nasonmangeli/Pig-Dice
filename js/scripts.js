5f4f91bd13a4196edb3845dd2f5ccd57f26433dd
nason mangeli <kuuchman@gmail.com>
2017-08-28 17:33:16 +0300

    Add Prototype for hold

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
-var diceRoll= function(){
-  return Math.floor(Math.random()*6)+1
+function resetFields() {
+  $("input#player1-userName").val("");
+  $("input#player2-userName").val("");
 }
+
 
 //Player constructor
  function Player(name, diceRoll, turnpoints, score){
@@ -10,4 +12,25 @@ var diceRoll= function(){
    this.score = 0;
  }
 
- 
+
+//Prototype for turn
+Player.prototype.rollDie = function() {
+
+  var diceRoll= Math.floor(Math.random()*6)+1
+
+  if (diceRoll === 1) {
+    this.turnpoints = 0;
+    alert("Sorry!! You got a 1. Your turn is over!!!!!")
+  } else {
+    this.turnpoints += diceRoll;
+  }
+  return diceRoll;
+};
+
+
+//Prototype for hold
+Player.prototype.hold = function() {
+  this.score += this.turnpoints;
+  this.totalScores = 0;
+  return this.score;
+};
