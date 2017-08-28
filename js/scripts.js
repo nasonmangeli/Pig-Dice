399ecb14f14307a8c6462e8785b8f71f5410a85a
nason mangeli <kuuchman@gmail.com>
2017-08-28 17:04:57 +0300

    Create Player constructor Change var die() to diceRoll()

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-var die = function(){
+var diceRoll= function(){
   return Math.floor(Math.random()*6)+1
 }
+
+//Player constructor
+ function Player(name, diceRoll, turnpoints, score){
+   this.name = name;
+   this.diceRoll = 0;
+   this.turnpoints = 0;
+   this.score = 0;
+ }
+
+ 
